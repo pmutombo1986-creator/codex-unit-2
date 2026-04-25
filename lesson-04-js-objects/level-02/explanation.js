d1d61d17e5a2e59de1812a6cd243723e450a9d29
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-24 21:07:07 -0400

    finished level 2 intro: what is an object

--- lesson-04-js-objects/level-02/explanation.js
+++ lesson-04-js-objects/level-02/explanation.js
@@ -1,2 +1,6 @@
 // TODO: Default-export a short explanation string mentioning "key" and "value".
-export default "TODO: Objects are collections of key/value pairs.";
+export default [
+   "Objects are collections of key/value pairs."
+
+];
+
