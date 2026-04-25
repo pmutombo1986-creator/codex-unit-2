7e5cd709452c2ba40565f6c7efefffb6daf25226
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-24 22:10:35 -0400

    finished level 4

--- lesson-04-js-objects/level-04/example.js
+++ lesson-04-js-objects/level-04/example.js
@@ -2,4 +2,8 @@
 // Example: export default { a: 1, b: "x" };
 export default {
   /* TODO: add keys and values */
+  id: 407,
+  username: "pmutombo_26",
+  status: "active",
+  profession: "coder_in_training"
 };
