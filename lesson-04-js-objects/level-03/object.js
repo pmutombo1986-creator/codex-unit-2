4afd892b0c33fb31cbbacfe910991eb0cde17179
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-24 21:16:57 -0400

    finished level 3 exporting objects

--- lesson-04-js-objects/level-03/object.js
+++ lesson-04-js-objects/level-03/object.js
@@ -2,5 +2,12 @@
 // Example: const user = { name: 'Ava' }; export default user;
 const studentObject = {
   /* TODO: add properties */
+
+  name: 'Pearla',
+  major: 'computer science',
+  level: 'Level 2',
+  book: 'Tuesdays with Morris'
+
 };
+
 export default studentObject;
