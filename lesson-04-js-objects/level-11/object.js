ed1887f2b68ffb00e4adeb44d8ae25016a7bf3f1
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-27 13:50:14 -0400

    finished level 11 build objects from descriptions

--- lesson-04-js-objects/level-11/object.js
+++ lesson-04-js-objects/level-11/object.js
@@ -1,6 +1,11 @@
 // TODO: Build an object from a written specification and export it as default.
 // Example spec: book with title, author, pages, and a summary() method assigned from a named function.
+function summary() {
+  return `${this.title} by ${this.author}`
+}
 const book = {
   /* TODO: implement spec */
+  title: 'The Great Gatspy', author: 'F.Scott Fitzgeral', pages: 120
 };
+book.summary = summary;
 export default book;
