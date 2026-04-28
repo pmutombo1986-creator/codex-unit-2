f5a4f0586af3bc1f3f962d175e7f93e1d3c3c69e
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-27 20:29:24 -0400

    finished level 4 current directory .

--- lesson-05-terminal-commands/level-04/answer.js
+++ lesson-05-terminal-commands/level-04/answer.js
@@ -1,3 +1,3 @@
 // Level 04 — Current directory (.)
 // TODO: Run a script using `./` and write a short note here mentioning `.` or `./`.
-export default "";
+export default " i used ./ to run answer.js. the. represents the current folder.";
