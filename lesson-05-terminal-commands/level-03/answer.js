fac5167207d4b7da354180c4a4b928ed2bd6ea34
Pearla Mutombo <pmutombo1986@gmail.com>
2026-04-27 20:24:06 -0400

    finsihed level 3 changed directory with cd

--- lesson-05-terminal-commands/level-03/answer.js
+++ lesson-05-terminal-commands/level-03/answer.js
@@ -1,3 +1,3 @@
 // Level 03 — Change directory with cd
 // TODO: Use `cd` to move into a folder and back; describe your commands here.
-export default "";
+export default "In the terminal, i switched to the level 3 folder with cd command. cd into level-03-folder and back";
